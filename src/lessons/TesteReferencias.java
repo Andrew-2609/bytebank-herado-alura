548473267de958daafa1a249657e1eedaff44899
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 12:02:16 -0300

    Feat: add basic implementation of TesteReferenciais.java

--- src/lessons/TesteReferencias.java
+++ src/lessons/TesteReferencias.java
@@ -1,4 +1,16 @@
 package lessons;
 
+import models.Gerente;
+
 public class TesteReferencias {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente();
+        gerente.setNome("Andrew Johnson");
+        gerente.setSalario(5000.0);
+
+        ControleBonificacao controleBonificacao = new ControleBonificacao();
+        controleBonificacao.registrarBonificacao(gerente);
+
+        System.out.println(controleBonificacao.getSoma());
+    }
 }
