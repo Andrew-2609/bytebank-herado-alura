8d23d87343766d58f1071ab0e914dd65f297374f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 12:00:20 -0300

    Feat: add basic implementation of ControleBonificacao.java

--- src/lessons/ControleBonificacao.java
+++ src/lessons/ControleBonificacao.java
@@ -1,4 +1,17 @@
 package lessons;
 
+import models.Gerente;
+
 public class ControleBonificacao {
+
+    private double soma;
+
+    public void registrarBonificacao(Gerente gerente) {
+        double bonificacao = gerente.getBonificacao();
+        this.soma += bonificacao;
+    }
+
+    public double getSoma() {
+        return this.soma;
+    }
 }
