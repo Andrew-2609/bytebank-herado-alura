57ce82d97262ae898ebbbb4fb890f507542aa8f8
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 11:04:22 -0300

    Feat: implement main method in TesteFuncionario

--- src/lessons/TesteFuncionario.java
+++ src/lessons/TesteFuncionario.java
@@ -1,4 +1,14 @@
 package lessons;
 
+import models.Funcionario;
+
 public class TesteFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Andrew Monteiro");
+        funcionario.setSalario(2000.0);
+
+        System.out.println(funcionario.getNome());
+        System.out.println(funcionario.getBonificacao());
+    }
 }
