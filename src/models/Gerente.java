0093f3b149cfc5a55d7351c1aabf6e88bf619ea3
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 11:27:16 -0300

    Chore: add basic implementation of subclass Gerente

--- src/models/Gerente.java
+++ src/models/Gerente.java
@@ -1,4 +1,13 @@
 package models;
 
-public class Gerente {
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
+    /*public double getBonificacao() {
+        return this.salario * 0.1;
+    }*/
 }
