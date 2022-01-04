9004509015c1e6c2313863c3df47f60799c73ef9
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 12:10:10 -0300

    Refactor: remove redundant registrarBonificacao() methods in ControleBonificacao.java

--- src/lessons/ControleBonificacao.java
+++ src/lessons/ControleBonificacao.java
@@ -1,25 +1,13 @@
 package lessons;
 
-import models.EditorDeVideos;
 import models.Funcionario;
-import models.Gerente;
 
 public class ControleBonificacao {
 
     private double soma;
 
-    public void registrarBonificacao(Gerente gerente) {
-        double bonificacao = gerente.getBonificacao();
-        this.soma += bonificacao;
-    }
-
     public void registrarBonificacao(Funcionario funcionario) {
         double bonificacao = funcionario.getBonificacao();
-        this.soma += bonificacao;
-    }
-
-    public void registrarBonificacao(EditorDeVideos editorDeVideos) {
-        double bonificacao = editorDeVideos.getBonificacao();
         this.soma += bonificacao;
     }
 
