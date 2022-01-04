dee8d4fb73b74d3bc4cf7f34a6fbb02a8e6ca60c
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 12:04:03 -0300

    Feat: add registrarBonificacao() method call for Funcionario object in TesteReferencias.java

--- src/lessons/TesteReferencias.java
+++ src/lessons/TesteReferencias.java
@@ -1,5 +1,6 @@
 package lessons;
 
+import models.Funcionario;
 import models.Gerente;
 
 public class TesteReferencias {
@@ -8,8 +9,13 @@ public class TesteReferencias {
         gerente.setNome("Andrew Johnson");
         gerente.setSalario(5000.0);
 
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Andrew Müller");
+        funcionario.setSalario(2000.0);
+
         ControleBonificacao controleBonificacao = new ControleBonificacao();
         controleBonificacao.registrarBonificacao(gerente);
+        controleBonificacao.registrarBonificacao(funcionario);
 
         System.out.println(controleBonificacao.getSoma());
     }
