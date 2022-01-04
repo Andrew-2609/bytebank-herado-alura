2162aa265b13d88419ea889d9a51c5d2b5ba2b89
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 11:31:41 -0300

    Feat: add basic implementation of TesteGerente.java

--- src/lessons/TesteGerente.java
+++ src/lessons/TesteGerente.java
@@ -1,4 +1,24 @@
 package lessons;
 
+import models.Gerente;
+
 public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente();
+        gerente.setNome("Marco");
+        gerente.setCpf("332.887.554-11");
+        gerente.setSalario(5000.0);
+
+        System.out.println(gerente.getNome());
+        System.out.println(gerente.getCpf());
+        System.out.println(gerente.getSalario());
+
+        gerente.setSenha(1234);
+
+        System.out.println(gerente.getSenha());
+
+        if (gerente.autenticar(4432)) {
+            System.out.println("As senhas batem.");
+        }
+    }
 }
