19d3191b11157c2bce4f65b5cfd59d861ac0115f
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 07:49:11 -0300

    Feat: implement methods of Autenticavel interface in Cliente.java

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,4 +1,17 @@
 package models;
 
-public class Cliente {
+public class Cliente implements Autenticavel {
+
+    private int senha;
+
+    @Override
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
 }
