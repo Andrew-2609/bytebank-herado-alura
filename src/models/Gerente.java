79dedb175a2d1c2591d1ceeb6c901ed071f07690
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 07:54:45 -0300

    Feat: implement methods of Autenticavel interface in Gerente.java

--- src/models/Gerente.java
+++ src/models/Gerente.java
@@ -1,7 +1,21 @@
 package models;
 
-public class Gerente extends Autenticavel {
+public class Gerente extends Funcionario implements Autenticavel {
+
+    private int senha;
+
     public double getBonificacao() {
         return super.getSalario();
     }
+
+    @Override
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
 }
