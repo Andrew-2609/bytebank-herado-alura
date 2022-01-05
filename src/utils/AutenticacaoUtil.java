f4976a851c8d079139d4ee9e3aa7d17ac996f70a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 09:00:06 -0300

    Feat: add implementation of AutenticacaoUtil.java

--- src/utils/AutenticacaoUtil.java
+++ src/utils/AutenticacaoUtil.java
@@ -1,4 +1,15 @@
 package utils;
 
 public class AutenticacaoUtil {
+
+    private int senha;
+
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
 }
