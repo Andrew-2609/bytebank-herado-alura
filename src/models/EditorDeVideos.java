e958d3ad8a8a87d2349041d4831cb5f6a43b48d1
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 22:01:46 -0300

    Chore: make getBonificacao() an abstract method in Funcionario.java

--- src/models/EditorDeVideos.java
+++ src/models/EditorDeVideos.java
@@ -3,7 +3,7 @@ package models;
 public class EditorDeVideos extends Funcionario {
 
     public double getBonificacao() {
-        return super.getBonificacao() + 100;
+        return 200;
     }
 
 }
