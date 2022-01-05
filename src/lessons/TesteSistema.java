3d3adee910369d9369ee9e692028138cbd00c90a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 07:37:22 -0300

    Feat: add basic implementation of TesteSistema.java

--- src/lessons/TesteSistema.java
+++ src/lessons/TesteSistema.java
@@ -1,4 +1,19 @@
 package lessons;
 
+import models.Administrador;
+import models.Gerente;
+import models.SistemaInterno;
+
 public class TesteSistema {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente();
+        gerente.setSenha(2222);
+
+        Administrador administrador = new Administrador();
+        administrador.setSenha(3333);
+
+        SistemaInterno sistemaInterno = new SistemaInterno();
+        sistemaInterno.autenticar(gerente);
+        sistemaInterno.autenticar(administrador);
+    }
 }
