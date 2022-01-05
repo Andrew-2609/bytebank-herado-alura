59d71ccc653fbbb8cd94960094deb1af25768d73
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-04 21:57:27 -0300

    Chore: make Funcionario an abstract class

--- src/lessons/TesteFuncionario.java
+++ src/lessons/TesteFuncionario.java
@@ -1,10 +1,11 @@
 package lessons;
 
 import models.Funcionario;
+import models.Gerente;
 
 public class TesteFuncionario {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario();
+        Funcionario funcionario = new Gerente();
         funcionario.setNome("Andrew Monteiro");
         funcionario.setSalario(2000.0);
 
--- src/lessons/TesteReferencias.java
+++ src/lessons/TesteReferencias.java
@@ -10,17 +10,12 @@ public class TesteReferencias {
         gerente.setNome("Andrew Johnson");
         gerente.setSalario(5000.0);
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Andrew Müller");
-        funcionario.setSalario(2000.0);
-
         EditorDeVideos editorDeVideos = new EditorDeVideos();
         editorDeVideos.setNome("Carlinhos Guilherme");
         editorDeVideos.setSalario(3000.0);
 
         ControleBonificacao controleBonificacao = new ControleBonificacao();
         controleBonificacao.registrarBonificacao(gerente);
-        controleBonificacao.registrarBonificacao(funcionario);
         controleBonificacao.registrarBonificacao(editorDeVideos);
 
         System.out.println(controleBonificacao.getSoma());
