f16408ab368810a31722afdaed6fa7f6da140fa7
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 07:35:37 -0300

    Feat: implement method 'autenticar' of SistemaInterno.java

--- src/models/SistemaInterno.java
+++ src/models/SistemaInterno.java
@@ -1,4 +1,16 @@
 package models;
 
 public class SistemaInterno {
+
+    private final int senha = 2222;
+
+    public void autenticar(FuncionarioAutenticavel funcionarioAutenticavel) {
+        boolean autenticou = funcionarioAutenticavel.autenticar(this.senha);
+        if (autenticou) {
+            System.out.println("Funcionário pode entrar no sistema!");
+        } else {
+            System.out.println("Funcionário não pode entrar no sistema!");
+        }
+    }
+
 }
