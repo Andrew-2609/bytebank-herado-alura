eff49583f1a39761b1139f7f92842dd08ed88eee
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-05 07:54:58 -0300

    Feat: implement methods of Autenticavel interface in Administrador.java

--- src/models/Administrador.java
+++ src/models/Administrador.java
@@ -1,8 +1,22 @@
 package models;
 
-public class Administrador extends Autenticavel {
+public class Administrador extends Funcionario implements Autenticavel {
+
+    private int senha;
+
     @Override
     public double getBonificacao() {
         return super.getSalario() / 2;
     }
+
+    @Override
+    public boolean autenticar(int senha) {
+        return this.senha == senha;
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
 }
