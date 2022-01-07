32d76834dfc92210309c6e82d7d86a0addb5caef
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-06 21:36:18 -0300

    Feat: implement a simple code to practice the immutability principle with Strings

--- src/lessons/TesteString.java
+++ src/lessons/TesteString.java
@@ -1,4 +1,13 @@
 package lessons;
 
 public class TesteString {
+    public static void main(String[] args) {
+        String nome = "Alura";
+        // String nome = new String("Alura");
+
+        String andAnotherOne = nome.replace('A', 'a');
+
+        System.out.println(nome);
+        System.out.println(andAnotherOne);
+    }
 }
