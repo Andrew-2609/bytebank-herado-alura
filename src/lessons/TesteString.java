c0c58e1967ae6ec9038a2ce106a357625c27bea4
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-06 21:52:50 -0300

    Feat: implement several ways of defining/using a char sequence with Java

--- src/lessons/TesteString.java
+++ src/lessons/TesteString.java
@@ -3,11 +3,20 @@ package lessons;
 public class TesteString {
     public static void main(String[] args) {
         String nome = "Alura";
-        // String nome = new String("Alura");
 
         String andAnotherOne = nome.replace('A', 'a');
 
         System.out.println(nome);
         System.out.println(andAnotherOne);
+
+        CharSequence sequence = "Here goes nothing...";
+        StringBuilder builder = new StringBuilder(sequence);
+        builder.append(" and now here's something?");
+        builder.setCharAt(builder.toString().indexOf('?'), '!');
+
+        @SuppressWarnings("StringOperationCanBeSimplified")
+        String result = new String(builder.toString());
+
+        System.out.println("Result: " + result);
     }
 }
